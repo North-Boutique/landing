6ac7c523c32c9833ecdd0d6edfbbdf3675d09bb3
Anton <antonmihail@gmail.com>
2023-03-16 19:07:28 -0400

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import "tachyons";
 import { Heading, Image } from "rebass";
@@ -10,7 +9,6 @@ import {
   ScrollDownIndicator
 } from "react-landing-page";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
